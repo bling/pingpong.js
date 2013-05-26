4a6df79d2ef2296b22fcc3087bc9cc3a5fc3a3aa
Bailey Ling <bling@live.ca>
2013-05-26 04:44:10 +0000

    add basic parsing of tweets

--- routes/stream.js
+++ routes/stream.js
@@ -1,8 +1,23 @@
+var _ = require('lodash')
 var fs = require('fs');
 var client = require('../modules/client');
 
 var t = new client();
 t.connect();
+
+function minify(tweet) {
+  var t = {};
+  _.each(['created_at','id_str','text'], function (key) {
+    t[key] = tweet[key];
+  });
+
+  t.user = {};
+  _.each(['name','screen_name','profile_image_url_https'], function(key) {
+    t.user.key = tweet.user.key;
+  })
+
+  return JSON.stringify(t);
+}
 
 exports.index = function(req, res) {
   res.render('stream');
@@ -16,7 +31,7 @@ exports.home = function(req, res) {
   });
   if (req.headers.accept == 'text/event-stream') {
     t.on('tweet', function(tweet) {
-      res.write('data:' + tweet.text);
+      res.write('data:' + minify(tweet));
       res.write('\n\n');
     });
   }
