b36e67719e42032d9ee0b03dd57928d4b87aa004
Bailey Ling <bling@live.ca>
2013-05-26 23:53:43 +0000

    remove more template stuff

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,22 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
-angular.module('pingpongApp', ['pingpongApp.filters', 'pingpongApp.services', 'pingpongApp.directives']).
-config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: MyCtrl1
-    });
-    $routeProvider.when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: MyCtrl2
-    });
-    $routeProvider.when('/stream', {
-      templateUrl: 'partials/stream',
-      controller: StreamCtrl
-    });
-    $routeProvider.otherwise({
-      redirectTo: '/view1'
-    });
-    $locationProvider.html5Mode(true);
-  }
-]);
+angular
+  .module('pingpongApp', ['pingpongApp.filters', 'pingpongApp.services', 'pingpongApp.directives'])
+  .config([
+    '$routeProvider',
+    '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider.when('/stream/home', {
+        templateUrl: 'partials/stream',
+        controller: StreamCtrl
+      });
+      $routeProvider.when('/stream/firehose', {
+        templateUrl: 'partials/stream',
+        controller: StreamCtrl
+      });
+      $routeProvider.otherwise({
+        redirectTo: '/'
+      });
+      $locationProvider.html5Mode(true);
+    }
+  ]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,23 +1,7 @@
 'use strict';
 
 function AppCtrl($scope, $http) {
-  $http({
-    method: 'GET',
-    url: '/api/name'
-  }).
-  success(function(data, status, headers, config) {
-    $scope.name = data.name;
-  }).
-  error(function(data, status, headers, config) {
-    $scope.name = 'Error!';
-  });
 }
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-function MyCtrl2() {}
-MyCtrl2.$inject = [];
 
 function TweetCtrl($scope) {
 }
